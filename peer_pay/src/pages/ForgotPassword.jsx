9d0f95eca2866f3d29e7df019602bc067ad56ab7
Jayitadey <jayitadey609@gmail.com>
2025-09-10 21:55:13 +0530

    commit

--- peer_pay/src/pages/ForgotPassword.jsx
+++ peer_pay/src/pages/ForgotPassword.jsx
@@ -34,7 +34,7 @@ function ForgotPassword() {
   const resetPassword = async () => {
     try {
       setloading(true);
-      const res = await fetch("http://localhost:5000/api/auth/reset-password", {
+      const res = await fetch("https://peer-pay-1.onrender.com/api/auth/reset-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, otp, newPassword }),
